405bf2724647d4c7de316c602a4901669ac74382
phanh260198 <53067948+phanh260198@users.noreply.github.com>
2019-11-11 12:23:27 +0700

    Update index.php

--- mycode/index.php
+++ mycode/index.php
@@ -1,10 +1,8 @@
 <?php 
 	//start session
 	session_start();
-	//include file config.php
-	include "config.php";
+	use mycode\config;
 	use mycode\app\connection;
 	use mycode\app\controller;
-	//include file routing.php de dinh tuyen url
-	include "app/routing.php";
+	use mycode\app\routing;
  ?>
